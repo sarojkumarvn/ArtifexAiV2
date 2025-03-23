34f1a702498d9c4efb6a7fcbb9831d8f202afac3
sarojkumar <sarojojha2457@gmail.com>
2025-03-23 23:57:00 +0530

    Trying deployement

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/ArtifexAIV2/", // ✅ Add your repo name as the base
 })
